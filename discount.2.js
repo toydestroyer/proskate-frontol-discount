14a970b954a317eb10d99b1ba0821e0441b727c9
Sergey Toy <toydestroyer@me.com>
2015-01-04 00:48:20 +0300

    Add alternative variant for discount

---
+++ discount.2.js
@@ -0,0 +1,31 @@
+function BeforeAct(AO, RO, E, O, CO) {}
+
+function AfterAct(AO, RO, E, O, CO) {}
+
+function FuncAct(AO, RO, CO) {}
+
+function NoAction(AO, RO, E, O, CO) {
+  var MinPos = null;
+  for (RO.Pos.Index = 1; RO.Pos.Index <= RO.Pos.Count; RO.Pos.Index++) {
+    if (RO.Pos.Storno) {
+      continue;
+    }
+    if (MinPos == null) {
+      MinPos = RO.Pos.Price;
+    } else {
+      if (MinPos > RO.Pos.Price) {
+        MinPos = RO.Pos.Price;
+      }
+    }
+  }
+  var count = 0;
+  for (RO.Pos.Index = 1; RO.Pos.Index <= RO.Pos.Count; RO.Pos.Index++) {
+    if (RO.Pos.Storno) {
+      continue;
+    }
+    count += RO.Pos.Quantity;
+  }
+  if (count >= 2) {
+    return MinPos/2;
+  }
+}
